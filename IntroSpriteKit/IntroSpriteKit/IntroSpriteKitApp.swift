ffec1d992f9a396cb1d541048a125553cf3c7c25
Dave Spina <dspina79@gmail.com>
2021-03-04 21:02:51 -0500

    Project initialization

---
+++ IntroSpriteKit/IntroSpriteKit/IntroSpriteKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroSpriteKitApp.swift
+//  IntroSpriteKit
+//
+//  Created by Dave Spina on 3/4/21.
+//
+
+import SwiftUI
+
+@main
+struct IntroSpriteKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
